b2623492a040692b74582182f1a440301dc26e63
xamcigam <1ich2ich3ich@gmail.com>
2020-04-26 18:43:12 +0200

    added testing cli main.go (does not build, package error)

--- cli/main.go
+++ cli/main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"github.com/xamcigam/VoltFuzer"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	var master *Series
+	var para *Parallel
+	var w1, w2, w3
+
+	master.childA = w1
+	w1.parent = master
+	master.childB = para
+	para.parent = master
+	para.childA = w2
+	w2.parent = para
+	para.childB = w3
+	w3.parent = para
+
+	fmt.Println(master.IsValid())
 }
